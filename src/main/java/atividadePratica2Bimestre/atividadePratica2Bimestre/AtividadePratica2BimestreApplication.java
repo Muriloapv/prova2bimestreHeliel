7da45e783a04e2c2aeec814f754869cd19dfabc0
Murilo Alves Pereira Varoto <66957888+Muriloapv@users.noreply.github.com>
2025-05-13 19:49:06 -0300

    commit inicial

---
+++ src/main/java/atividadePratica2Bimestre/atividadePratica2Bimestre/AtividadePratica2BimestreApplication.java
@@ -0,0 +1,13 @@
+package atividadePratica2Bimestre.atividadePratica2Bimestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadePratica2BimestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadePratica2BimestreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atividadePratica2Bimestre/atividadePratica2Bimestre/AtividadePratica2BimestreApplicationTests.java
@@ -0,0 +1,13 @@
+package atividadePratica2Bimestre.atividadePratica2Bimestre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadePratica2BimestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
